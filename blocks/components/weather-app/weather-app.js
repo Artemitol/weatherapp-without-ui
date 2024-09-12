50ca09a01b2c026e435f3b172f28461926792017
Artemitol <artemiy.averin@gmail.com>
2024-09-12 14:59:10 +0300

    added class to the weather-app.js

--- blocks/components/weather-app/weather-app.js
+++ blocks/components/weather-app/weather-app.js
@@ -238,26 +238,64 @@ const weatherCa = class weather {
     _temp = null
     _tempMax = null
     _tempMin = null 
-
-    constructor() {
-
-    }
+    _isExisting = false
+    _cityName = null
 
 
     //  Geters and seters
     get temp() {
         return this._temp
     }
+    set temp(value) {
+        const temperature = document.getElementById("temperature")
+        temperature.textContent = value
+
+        this._temp = value
+    }
 
     get tempMin() {
         return this._tempMin
+    }
+    set tempMin(value) {
+        const temperature = document.getElementById("temperature-min")
+        temperature.textContent = value
+
+        this._tempMin = value
     }
     
     get tempMax() {
         return this._tempMax
     }
+    set tempMax(value) {
+        const temperature = document.getElementById("temperature-max")
+        temperature.textContent = value
+
+        this._tempMax = value
+    }
+
+    get cityName() {
+        return this._cityName
+    }
+    set cityName(value) {
+        const cityName = document.getElementById("city-name")
+        cityName.textContent = value
+
+        this._cityName = value
+    }
 
 
     // Functions
-    
+    show() {
+        const card = document.getElementById("weather-card")
+        card.classList.remove("hidden")
+
+        this._isExisting = true
+    }
+
+    hide() {
+        const card = document.getElementById("weather-card")
+        card.classList.add("hidden")
+
+        this._isExisting = false
+    }
 }
